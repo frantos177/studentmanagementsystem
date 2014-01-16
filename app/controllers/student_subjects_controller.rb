fd20588ec6cb2bd72cd6c69880730c8ff4f06597
Ephraim Santos <ephraim.ryan.santos@gmail.com>
2014-01-16 23:03:48 +0800

    Fixed StudentSubject controller naming

--- app/controllers/student_subjects_controller.rb
+++ app/controllers/student_subjects_controller.rb
@@ -1,36 +1,36 @@
 class StudentSubjectsController < ApplicationController
 
 	def index
-		@student_subjects = Student_subject.all
+		@student_subjects = StudentSubject.all
 	end
 
 	def new
-		@student_subject = Student_subject.new
+		@student_subject = StudentSubject.new
 	end
 
 	def create
-		@student_subject = Student_subject.new(student_subject_params)
+		@student_subject = StudentSubject.new(student_subject_params)
 		@student_subject.save
 		redirect_to student_subjects_path
 	end
 
 	def edit
-		@student_subject = Student_subject.find(params[:id])
+		@student_subject = StudentSubject.find(params[:id])
 	end
 
 	def update
-		@student_subject = Student_subject.find(params[:id])
+		@student_subject = StudentSubject.find(params[:id])
 		@student_subject.update_attributes(student_subject_params)
 		redirect_to student_subjects_path
 	end
 
 	def destroy
-		Student_subject.find(params[:id]).destroy
+		StudentSubject.find(params[:id]).destroy
 		redirect_to student_subjects_path
 	end
 
 	def show
-		@student_subject = Student_subject.find(params[:id])
+		@student_subject = StudentSubject.find(params[:id])
 	end
 
 	private 
